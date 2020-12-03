4f217d849e225531e40101d6e66f36bcafcca927
Jack - <jackmerry123@hotmail.com>
2020-12-03 11:10:25 +0000

    Create Data Structures

---
+++ script.js
@@ -0,0 +1,4 @@
+const messagesOpening = ["The smallest ", "The biggest ", "The oldest "];
+const messageMiddles = ["city in the UK is", "country by land area is ", "planet in our solar system is"];
+const endings = [[" St Davids.", " London.", "York."], ["Vatican City.", "Russa.", "Egypt."], ["Mercury.", "Jupiter."]];
+
