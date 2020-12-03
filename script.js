aea01daee203685c28c69efab1c805419f52f23a
Jack - <jackmerry123@hotmail.com>
2020-12-03 12:13:32 +0000

    Working message composition

--- script.js
+++ script.js
@@ -1,4 +1,53 @@
-const messagesOpening = ["The smallest ", "The biggest ", "The oldest "];
-const messageMiddles = ["city in the UK is", "country by land area is ", "planet in our solar system is"];
-const endings = [[" St Davids.", " London.", "York."], ["Vatican City.", "Russa.", "Egypt."], ["Mercury.", "Jupiter."]];
+const messageOpening = ["The smallest ", "The biggest ", "The oldest "];
+const messageMiddles = ["city in the UK is", "country is", "planet in our solar system is"];
+const endings = [[" St Davids.", " London.", "York."], [" Vatican City.", " Russa.", " Egypt."], [" Mercury.", " Jupiter."]];
 
+const getRandomNumber = () => Math.floor(Math.random()*3);
+
+
+const startUpFunction = () => {
+    const rand = getRandomNumber();
+    const randTwo = getRandomNumber();
+    let finalMessage = messageOpening[rand].concat(messageMiddles[randTwo]);
+    console.log(rand);
+    console.log(randTwo);
+    let finalSegment;
+    switch(randTwo){
+        case 0: 
+            if(rand === 0){
+                finalSegment = endings[0][0];
+                
+            }else if(rand === 1){
+                finalSegment = endings[0][1];
+
+            }else if(rand === 2){
+                finalSegment = endings[0][2];
+
+            }
+
+            break;
+        case 1:
+            if(rand === 0){
+                finalSegment = endings[1][0];
+            }else if(rand === 1){
+                finalSegment = endings[1][1];
+            }else if(rand === 2){
+                finalSegment = endings[1][2];         
+            }
+
+            break;
+        case 2: 
+            if(rand === 0){
+                finalSegment = endings[2][0];
+            }else if(rand === 1){
+                finalSegment = endings[2][1];
+            }else if(rand === 2){
+                finalSegment = endings[2][1];
+            }
+
+            break;
+    }
+    return `${finalMessage}${finalSegment}`;
+}
+
+console.log(startUpFunction());
